d59ca8cae253234f22316b750d42b20197461204
Rajat Karahe <29karaherajat@gmail.com>
2020-05-20 23:21:08 +0530

    Adding version 3, with featured like loading image and getting result on a textview after going through the model

---
+++ BlindAssist_V03/app/src/main/java/com/example/helloworld/ModelClasses.java
@@ -0,0 +1,7 @@
+package com.example.helloworld;
+
+public class ModelClasses {
+    public static String[] MODEL_CLASSES = new String[]{
+            "10", "100", "20", "200", "2000", "5", "50", "500", "none"
+    };
+}
