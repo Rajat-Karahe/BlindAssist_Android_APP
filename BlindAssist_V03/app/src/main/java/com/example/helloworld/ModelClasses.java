8b1aa749ead58de3b9be3918814f06bd686cc7f2
Rajat Karahe <29karaherajat@gmail.com>
2020-05-24 22:33:08 +0530

    just v4

--- BlindAssist_V03/app/src/main/java/com/example/helloworld/ModelClasses.java
+++
@@ -1,7 +0,0 @@
-package com.example.helloworld;
-
-public class ModelClasses {
-    public static String[] MODEL_CLASSES = new String[]{
-            "10", "100", "20", "200", "2000", "5", "50", "500", "none"
-    };
-}
